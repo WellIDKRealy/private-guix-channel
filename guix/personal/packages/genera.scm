25d8ff12a4be817773dca2c5547d7dc171e81cc3
Maciej Kalandyk <m.kalandyk@outlook.com>
2023-11-24 03:06:56 +0100

    removed genera

--- guix/personal/packages/genera.scm
+++
@@ -1,8 +0,0 @@
-(define-module (personal packages genera)
-  #:use-module (guix packages)
-  #:use-module (guix utils)
-  #:use-module (guix gexp)
-  #:use-module (guix download)
-  #:use-module (guix status)
-  #:use-module (guix build-system trivial)
-  #:use-module (guix licenses))
