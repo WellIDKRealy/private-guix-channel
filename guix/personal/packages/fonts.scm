9f54425fcfe9c38ed17a6ec00ae5d6f7134e5e39
Maciej Kalandyk <m.kalandyk@outlook.com>
2023-11-25 03:19:00 +0100

    Add oldschool-pc-fonts package

---
+++ guix/personal/packages/fonts.scm
@@ -0,0 +1,22 @@
+(define-module (personal packages fonts)
+  #:use-module (guix package)
+  #:use-module (guix download)
+  #:use-module (guix build-system font)
+  #:use-module ((guix licenses)
+		#:prefix license:))
+
+(define-public oldshool-pc-fonts
+  (package
+   (name "font-flexi-ibm-vga")
+   (version "2.0")
+   (source (origin
+	    (method url-fetch)
+	    (uri "https://int10h.org/oldschool-pc-fonts/download/oldschool_pc_font_pack_v2.2_linux.zip")
+	    (sha256
+	     (base32 "0nfpld0in6a3fsmq5d8i35587j0k3387slg7igfx46lkr7nc63dk"))))
+   (build-system font-build-system)
+   (home-page "https://int10h.org/oldschool-pc-fonts/")
+   (synopsis "Old school pc fonts")
+   (description "The world's biggest collection of classic text mode fonts, system fonts and BIOS fonts from DOS-era IBM PCs and compatibles - preserving raster typography from pre-GUI times")
+   (license license:gpl3+)))
+
