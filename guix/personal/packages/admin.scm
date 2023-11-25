01b33a6b743a8c4fd51939cdc99eef0b26397672
Maciej Kalandyk <m.kalandyk@outlook.com>
2023-11-25 20:31:59 +0100

    Fix indentation in admin.scm

--- guix/personal/packages/admin.scm
+++ guix/personal/packages/admin.scm
@@ -9,22 +9,22 @@
 
 (define-public network-convinience-commands
   (package
-    (name "network-convinience-commands")
-    (version "0.1")
-    (source (origin
-	      (method git-fetch)
-	      (uri (git-reference
-		    (url "https://github.com/WellIDKRealy/network-convinience-commands.git")
-		    (commit "91c742a305a62edd857511ecb6b69fbf198edeb9")))
-	      (sha256
-	       (base32 "14pdi0kn71p139vx503kj70grq8938rnh2rdvrq1dw062g2q9s8p"))))
-    (build-system copy-build-system)
-    (arguments
-     '(#:install-plan
-       '(("cnet" "bin/")
-	 ("rnet" "bin/")
-	 ("enet" "bin/"))))
-    (home-page "https://github.com/WellIDKRealy/network-convinience-commands.git")
-    (synopsis "Convenience commands for dealing with faulty network")
-    (description "Commands that are commonly run when dealing with faulty network stack")
-    (license license:gpl3)))
+   (name "network-convinience-commands")
+   (version "0.1")
+   (source (origin
+	    (method git-fetch)
+	    (uri (git-reference
+		  (url "https://github.com/WellIDKRealy/network-convinience-commands.git")
+		  (commit "91c742a305a62edd857511ecb6b69fbf198edeb9")))
+	    (sha256
+	     (base32 "14pdi0kn71p139vx503kj70grq8938rnh2rdvrq1dw062g2q9s8p"))))
+   (build-system copy-build-system)
+   (arguments
+    '(#:install-plan
+      '(("cnet" "bin/")
+	("rnet" "bin/")
+	("enet" "bin/"))))
+   (home-page "https://github.com/WellIDKRealy/network-convinience-commands.git")
+   (synopsis "Convenience commands for dealing with faulty network")
+   (description "Commands that are commonly run when dealing with faulty network stack")
+   (license license:gpl3)))
